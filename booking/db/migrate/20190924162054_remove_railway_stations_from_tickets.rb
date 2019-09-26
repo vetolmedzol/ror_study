5746ed6facccb820a39d7609dcb1aa981c9789c7
vetolmedzol <vetolmedzol@gmail.com>
2019-09-26 10:14:34 +0300

    TeachBase 19 ready to Heroku 3 db drop

--- booking/db/migrate/20190924162054_remove_railway_stations_from_tickets.rb
+++
@@ -1,7 +0,0 @@
-class RemoveRailwayStationsFromTickets < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tickets, :first_railway_station_id_id
-    remove_column :tickets, :last_railway_station_id_id
-    remove_column :tickets, :railway_station_id
-  end
-end
--- booking/db/schema.rb
+++ booking/db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_162424) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "number"
+    t.bigint "railway_station_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_162424) do
     t.bigint "last_railway_station_id"
     t.index ["first_railway_station_id"], name: "index_tickets_on_first_railway_station_id"
     t.index ["last_railway_station_id"], name: "index_tickets_on_last_railway_station_id"
+    t.index ["railway_station_id"], name: "index_tickets_on_railway_station_id"
     t.index ["train_id"], name: "index_tickets_on_train_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
